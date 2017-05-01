18ea0b7c5ab8c4e4d2c2c5c01af822ab3d5b1f05
ic16b077 <michael.kaefer@technikum-wien.at>
2017-05-01 13:55:29 +0200

    doxyygen

--- mypopen.h
+++ mypopen.h
@@ -1,16 +1,36 @@
+/**
+ * @file mypopen.c
+ * Betriebssysteme Mypopen Header File.
+ * Beispiel 2
+ *
+ * @author Aleksandar Pavlovic, Johanna Hoffmann, Michael Käfer
+ * @date 2017/04/30
+ *
+ * @version 1
+ *
+ * @todo Review
+ *
+ */
 
+/*
+ * --------------------------------------------------------------- defines --
+ */
 #ifndef MYPOPEN_H
 #define MYPOPEN_H
 
-
-/* includes */
+/*
+ * -------------------------------------------------------------- includes --
+ */
 #include <stdio.h>
 
-/* prototypes */
-
+/*
+ * ------------------------------------------------------------- functions --
+ */
 extern FILE *mypopen(const char *command, const char *type);
 extern int mypclose(FILE *stream);
 
 #endif
 
-/* EOF */
+/*
+ * =================================================================== eof ==
+ */
